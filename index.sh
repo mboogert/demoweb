fed3e7a65cd6fcdfba0baa361b5d0b8ac19a96c7
Marcel Boogert <marcel@mtdb.nl>
2017-01-16 21:37:05 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@
 BGCOLOR="grey"
 
 if [ -z "$SITE_NAME" ]; then
-    echo "<html><body bgcolor=$BGCOLOR><h1>NO VALUE SUBMITTED FOR VARIABLE SITE_NAME</h1></body></html>" > /usr/share/nginx/html/index.htm
+    echo "<html><body bgcolor=$BGCOLOR><h1>NO VALUE SUBMITTED FOR VARIABLE SITE_NAME</h1></body></html>" > /usr/share/nginx/html/index.html
 else
-    echo "<html><body bgcolor=$BGCOLOR><h1>$SITE_NAME</h1></body></html>" > /usr/share/nginx/html/index.htm
+    echo "<html><body bgcolor=$BGCOLOR><h1>$SITE_NAME</h1></body></html>" > /usr/share/nginx/html/index.html
 fi
