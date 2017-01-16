7d7f5ed66a9e415327663679b72adde7ad13e834
Marcel Boogert <marcel@mtdb.nl>
2017-01-16 21:18:39 +0100

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+BGCOLOR="grey"
+
+if [ -z "$SITE_NAME" ]; then
+    echo "<html><body bgcolor=$BGCOLOR><h1>NO VALUE SUBMITTED FOR VARIABLE SITE_NAME</h1></body></html>" > /usr/share/nginx/html/index.htm
+else
+    echo "<html><body bgcolor=$BGCOLOR><h1>$SITE_NAME</h1></body></html>" > /usr/share/nginx/html/index.htm
+fi
